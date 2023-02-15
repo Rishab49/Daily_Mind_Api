8ac668e6bd1b72738493416f3d2d6b6b05cb16b1
Rishab Raj <rishab.rishu49@gmail.com>
2023-02-15 10:25:07 +0530

    converted origin to function

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const cors = require("cors");
 const app = express();
 
 
+const allowlist=["http://localhost:5173","https://deep-mind.vercel.app"]
+
 //cors options
 const corsOptions = {
-origin:["http://localhost:5173","https://deep-mind.vercel.app"],
-methods:["GET","POST"],
-allowedHeaders: ['Content-Type']
+origin:function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 }
 
 
